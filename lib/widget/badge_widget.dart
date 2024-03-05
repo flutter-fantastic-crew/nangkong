783723735bf66c52cd36fa24e97d4d56edd48555
nangkong <godtjrdl98@gmail.com>
2024-03-05 19:20:44 +0900

    bottomSheet 추가~!

--- lib/widget/badge_widget.dart
+++ lib/widget/badge_widget.dart
@@ -1,20 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:nangkong/widget/summary_bottom_sheet_widget.dart';
 
-class BadgeWidget extends Container {
-  BadgeWidget({required this.badgeText})
-      : super(
-    height: 28,
-    width: double.infinity,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(50),
-      color: Color(0xffB1B1B1),
-    ),
-    alignment: Alignment.center,
-    child: Text(
-      badgeText,
-      style: TextStyle(fontSize: 15, color: Color(0xffffffff)),
-    ),
-  );
-
+class BadgeWidget extends StatelessWidget {
   final String badgeText;
+  final VoidCallback? onTap;
+
+  BadgeWidget({required this.badgeText, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        SummaryBottomSheetWidget.show(context);
+      },
+      child: Container(
+        height: 28,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Color(0xffB1B1B1),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          badgeText,
+          style: TextStyle(fontSize: 15, color: Color(0xffffffff)),
+        ),
+      ),
+    );
+  }
 }
---
+++ lib/widget/summary_bottom_sheet_widget.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class SummaryBottomSheetWidget {
+  static void show(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true, // 높이 전체를 사용하는 true
+      builder: (BuildContext context) {
+        return _buildSummaryBottomSheetContent(context);
+      },
+    );
+  }
+
+  static Widget _buildSummaryBottomSheetContent(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double sheetHeight = screenHeight * 0.9; // 90%의 높이
+
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      height: sheetHeight,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Icon(Icons.horizontal_rule),
+          // TODO: 위로...더 붙이고 싶다
+          // Row(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   children: [
+          //     Icon(Icons.horizontal_rule),
+          //   ],
+          // ),
+
+          // 더미데이터
+          Text(
+            '요약 정보',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(height: 10),
+          ListTile(
+            leading: Icon(Icons.calendar_today),
+            title: Text('일정 요약'),
+            onTap: () {
+              // 일정 요약 로직을 추가할 수 있습니다.
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.pie_chart),
+            title: Text('통계 요약'),
+            onTap: () {
+              // 통계 요약 로직을 추가할 수 있습니다.
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
